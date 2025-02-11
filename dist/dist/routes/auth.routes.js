607664d09bd2a6e3a89c64bae6c125ca18d15227
Agustin Yedro <76633203+agustinyedro@users.noreply.github.com>
2025-02-10 22:44:10 -0300

    se realiza por segundo vez buidl

--- dist/dist/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-var _express = _interopRequireDefault(require("express"));
-var _auth = _interopRequireDefault(require("../controllers/auth"));
-var router = _express["default"].Router();
-router.post("/signup", _auth["default"].signUp);
-router.post("/signin", _auth["default"].signIn);
-module.exports = router;
