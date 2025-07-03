700e34869ce654cbeed22a98575e90b5ea33c68f
Agustin Yedro <76633203+agustinyedro@users.noreply.github.com>
2025-07-03 10:32:43 -0300

    se crea la rama dev

--- src/components/auth/auth.routes.js
+++ src/components/auth/auth.routes.js
@@ -1,8 +1,8 @@
-import { Router } from 'express';
-import authentication from '@auth/authController';
+import { Router } from "express";
+import authentication from "@auth/authController";
 
 const router = Router();
 
 router.post("/signup", authentication.signUp);
-router.post("/signin", authentication.signIn)
+router.post("/signin", authentication.signIn);
 module.exports = router;
