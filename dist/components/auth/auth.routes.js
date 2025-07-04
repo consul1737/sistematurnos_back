0c534e712d404fd2a3efba28e99d5226db17b710
Agustin Yedro <76633203+agustinyedro@users.noreply.github.com>
2025-07-04 10:41:40 -0300

    p

--- dist/components/auth/auth.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-var _express = require("express");
-var _authController = _interopRequireDefault(require("./authController"));
-var router = (0, _express.Router)();
-router.post("/signup", _authController["default"].signUp);
-router.post("/signin", _authController["default"].signIn);
-module.exports = router;
