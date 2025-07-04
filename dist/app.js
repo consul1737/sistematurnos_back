b7d2ded24e84ae061c32737ae6bbcaea94e8bcd1
Agustin Yedro <76633203+agustinyedro@users.noreply.github.com>
2025-07-04 11:01:19 -0300

    se agrega public en dist

--- dist/app.js
+++ dist/app.js
@@ -6,20 +6,40 @@ var _morgan = _interopRequireDefault(require("morgan"));
 require("module-alias/register");
 var _cors = _interopRequireDefault(require("cors"));
 var _expressFileupload = _interopRequireDefault(require("express-fileupload"));
-var _connectHistoryApiFallback = _interopRequireDefault(require("connect-history-api-fallback"));
+var _connectHistoryApiFallback = _interopRequireDefault(
+  require("connect-history-api-fallback")
+);
 var _path = _interopRequireDefault(require("path"));
 require("dotenv/config");
 var _auth = _interopRequireDefault(require("./components/auth/auth.routes"));
-var _whatsapp = _interopRequireDefault(require("./components/whatsapp/whatsapp.routes"));
-var _turnos = _interopRequireDefault(require("./components/turnos/turnos.routes"));
-var _pacientes = _interopRequireDefault(require("./components/pacientes/pacientes.routes"));
-var _consultorios = _interopRequireDefault(require("./components/consultorios/consultorios.routes"));
-var _tratamientos = _interopRequireDefault(require("./components/tratamientos/tratamientos.routes"));
+var _whatsapp = _interopRequireDefault(
+  require("./components/whatsapp/whatsapp.routes")
+);
+var _turnos = _interopRequireDefault(
+  require("./components/turnos/turnos.routes")
+);
+var _pacientes = _interopRequireDefault(
+  require("./components/pacientes/pacientes.routes")
+);
+var _consultorios = _interopRequireDefault(
+  require("./components/consultorios/consultorios.routes")
+);
+var _tratamientos = _interopRequireDefault(
+  require("./components/tratamientos/tratamientos.routes")
+);
 var _caja = _interopRequireDefault(require("./components/caja/caja.routes"));
-var _ventas = _interopRequireDefault(require("./components/ventas/ventas.routes"));
-var _articulos = _interopRequireDefault(require("./components/articulos/articulos.routes"));
-var _perfiles = _interopRequireDefault(require("./components/perfiles/perfiles.routes"));
-var _ubicacion = _interopRequireDefault(require("./components/ubicacion/ubicacion.routes"));
+var _ventas = _interopRequireDefault(
+  require("./components/ventas/ventas.routes")
+);
+var _articulos = _interopRequireDefault(
+  require("./components/articulos/articulos.routes")
+);
+var _perfiles = _interopRequireDefault(
+  require("./components/perfiles/perfiles.routes")
+);
+var _ubicacion = _interopRequireDefault(
+  require("./components/ubicacion/ubicacion.routes")
+);
 // Importar rutas
 
 // import contactosRoute from "./components/contactos/contactos.routes";
@@ -30,12 +50,16 @@ var app = (0, _express["default"])();
 app.use((0, _morgan["default"])("tiny"));
 app.use((0, _cors["default"])());
 app.use(_express["default"].json());
-app.use(_express["default"].urlencoded({
-  extended: true
-}));
-app.use((0, _expressFileupload["default"])({
-  useTempFiles: true
-}));
+app.use(
+  _express["default"].urlencoded({
+    extended: true,
+  })
+);
+app.use(
+  (0, _expressFileupload["default"])({
+    useTempFiles: true,
+  })
+);
 
 // Rutas
 app.use("/", _auth["default"]);
@@ -53,7 +77,9 @@ app.use("/ubicacion", _ubicacion["default"]);
 
 // Middleware para frontend
 app.use((0, _connectHistoryApiFallback["default"])());
-app.use(_express["default"]["static"](_path["default"].join(__dirname, "public")));
+app.use(
+  _express["default"]["static"](_path["default"].join(__dirname, "public"))
+);
 console.log("App.js se está ejecutando...");
 
 // Puerto
